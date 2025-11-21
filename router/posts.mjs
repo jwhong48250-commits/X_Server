a9b7f04d108ddecd44f3651bebf17e9c00a9cbbd
홍정우 <jwhong441@naver.com>
2025-11-21 14:34:37 +0900

    validata적용

--- router/posts.mjs
+++ router/posts.mjs
@@ -2,8 +2,15 @@
 
 import express from "express";
 import * as postController from "../controller/post.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 
 const router = express.Router();
+
+const validatePost = [
+  body("text").trim().isLength({ min: 4 }).withMessage("최소 4자 이상 입력"),
+  validate,
+];
 
 // 전체 포스트 가져오기
 // 특정 아이디에 대한 포스트 가져오기
@@ -17,14 +24,15 @@ router.get("/:id", postController.getPost);
 
 // 포스트 쓰기
 // HTTP://127.0.0.1:8000/post/
-router.post("/", postController.createPost);
+router.post("/", validatePost, postController.createPost);
 
 // 포스트 수정하기
 // HTTP://127.0.0.1:8000/post/:id
-router.put("/:id", postController.updataePost);
+router.put("/:id", validatePost, postController.updataePost);
 
 // 포스트 삭제하기
 // HTTP://127.0.0.1:8000/post/:id
 router.delete("/:id", postController.deletePost);
 
 export default router;
+// npm i express-validator

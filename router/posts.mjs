4aa71d56e235a9e0cd4e2c70e9c595d53b83c46c
홍정우 <jwhong441@naver.com>
2025-11-21 11:14:35 +0900

    API Test

--- router/posts.mjs
+++ router/posts.mjs
@@ -1,16 +1,30 @@
 // 글 관련한거
 
 import express from "express";
+import * as postController from "../controller/post.mjs";
 
 const router = express.Router();
+
+// 전체 포스트 가져오기
 // 특정 아이디에 대한 포스트 가져오기
+// HTTP://127.0.0.1:8000/post
+// HTTP://127.0.0.1:8000/post?userid=XXX
+router.get("/", postController.getPosts);
 
 // 글 번호에 대한 포스트 가져오기
+// HTTP://127.0.0.1:8000/post/:id
+router.get("/:id", postController.getPost);
 
 // 포스트 쓰기
+// HTTP://127.0.0.1:8000/post/
+router.post("/", postController.createPost);
 
 // 포스트 수정하기
+// HTTP://127.0.0.1:8000/post/:id
+router.put("/:id", postController.updataePost);
 
 // 포스트 삭제하기
+// HTTP://127.0.0.1:8000/post/:id
+router.delete("/:id", postController.deletePost);
 
 export default router;

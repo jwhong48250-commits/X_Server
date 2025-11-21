c35c107f6ab5846818d3b35ab708c235c05d14f5
홍정우 <jwhong441@naver.com>
2025-11-21 09:35:26 +0900

    초기 셋팅

---
+++ router/auth.mjs
@@ -0,0 +1,13 @@
+// 사용자 정보에 관한거
+
+import express from "express";
+
+const router = express.Router();
+
+// 회원가입
+
+// 로그인
+
+// 로그인 유지
+
+export default router;
---
+++ router/posts.mjs
@@ -0,0 +1,16 @@
+// 글 관련한거
+
+import express from "express";
+
+const reuter = express.Router();
+// 특정 아이디에 대한 포스트 가져오기
+
+// 글 번호에 대한 포스트 가져오기
+
+// 포스트 쓰기
+
+// 포스트 수정하기
+
+// 포스트 삭제하기
+
+export default router;

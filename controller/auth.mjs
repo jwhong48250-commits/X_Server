6a94f230f5fb540f5f3ecebc3b7074a9529ad4d8
홍정우 <jwhong441@naver.com>
2025-11-24 10:15:32 +0900

    .env 환경변수 추가

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -1,31 +1,27 @@
+import express from "express";
 import * as authRepository from "../data/auth.mjs";
 import * as bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
-const secretKey = "abcdefg1234!@#$";
-const bcryptSaltRounts = 10;
-const jwtExpiresInDays = "2d";
-
+import { config } from "../config.mjs";
 async function createJwtToken(id) {
-  return jwt.sign({ id }, secretKey, { expiresIn: jwtExpiresInDays });
+  return jwt.sign({ id }, config.jwt.secretKey, {
+    expiresIn: config.jwt.expiresInSec,
+  });
 }
-// 회원가입 함수
 export async function signup(req, res, next) {
   const { userid, password, name, email } = req.body;
-  //회원 중복 체크
+  // 회원 중복 체크
   const found = await authRepository.findByUserid(userid);
   if (found) {
     return res.status(409).json({ message: `${userid}이 이미 있습니다` });
   }
-
-  const hashed = bcrypt.hashSync(password, bcryptSaltRounts);
+  const hashed = bcrypt.hashSync(password, config.bcrypt.saltRounds);
   const user = await authRepository.createUser(userid, hashed, name, email);
   //   const user = await authRepository.createUser(userid, password, name, email);
   const token = await createJwtToken(user.id);
   console.log(token);
   res.status(201).json({ token, user });
 }
-// 로그인 함수
 export async function login(req, res, next) {
   const { userid, password } = req.body;
   const user = await authRepository.findByUserid(userid);
@@ -36,16 +32,14 @@ export async function login(req, res, next) {
   if (!isValidPassword) {
     return res.status(401).json({ message: `아이디 또는 비밀번호 확인` });
   }
-
   const token = await createJwtToken(user.id);
   res.status(200).json({ token, user });
 }
-
 export async function me(req, res, next) {
   //   const user = await authRepository.findByUserid(req.id);
   //   if (!user) {
-  //     return res.status(404).json({ message: "일치하는 사용가자 없음" });
+  //     return res.status(404).json({ message: "일치하는 사용자가 없음" });
   //   }
   //   res.status(200).json({ token: req.token, userid: user.userid });
-  res.status(200).json({ message: "성공했어~" });
+  res.status(200).json({ message: "성공했어 ~" });
 }
